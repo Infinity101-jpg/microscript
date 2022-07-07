6057e070b440a79b56af779e1c3898c5685e6b3e
Space <68457668+Infinity101-jpg@users.noreply.github.com>
2022-07-07 21:05:24 +0200

    Create asteroids.js
    has almost all functions of microscript except printresp and a good loop

---
+++ asteroids.js
@@ -0,0 +1,55 @@
+
+new::pos::2
+
+;;led.plot(0, pos)
+
+forever
+	new::ast::4
+	if::pos::===::5
+		change::pos::0
+	end
+
+	if::pos::===::-1
+		change::pos::4
+	end
+
+	;;function bta() {
+		if::btn_a
+			change::pos::pos+1;
+			;;led.unplot(0, pos - 1)
+		end
+	end
+
+	;;function btb() {
+		if::btn_b
+			change::pos::pos-1
+			;;led.unplot(0, pos + 1)
+		end
+	end
+
+	new::rand::randint(0, 4)
+	loop::5
+
+		if::pos::===::rand
+			
+			if::ast::===::0
+				printf::"game over"
+
+				forever
+					printf::" "
+				end
+			end
+
+
+		end
+
+		;;bta()
+		;;btb()
+		change::ast::ast-1
+		;;led.plot(0, pos)
+		;;led.plot(ast + 1, rand)
+		;;basic.pause(100)
+		;;led.unplot(ast + 2, rand)
+		;;led.unplot(0, rand)
+	end
+end
